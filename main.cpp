387905e496e27b9fe019f3107fa671afea72c5f3
Sergey Larshin <serg1994225@gmail.com>
2021-01-03 19:28:46 +0300

    Its work

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "CAST256.h"
+#include <cstring>
+int main(){
+	std::string key = "qwertyuiopasdfghjklzxcvbnmqwerty";
+	std::cout << key.length()<<"\n";
+	CAST256 test = CAST256(key);
+	std::cout << test.decrypt(test.encrypt("Hello, world!!!L")) << "\n";
+	return 0;
+}
